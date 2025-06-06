8340423fc93b0967650e8e4bae57ab5038ef5e9b
Mixipixi91 <mixipixi91@gmail.com>
2025-06-06 17:12:47 +0200

    connection update

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = createPool({
+export const pool = createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
-export const connection = pool.getConnection();
